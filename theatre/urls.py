45df577c99b9602a9dd8d5041e06234fd5bd49a5
Yuliia <verbikyullya@gmail.com>
2024-01-02 17:54:32 +0200

    add basic viewsets for Genre, Actor, TheatreHall, Play, Performance models and configure urls for them

--- theatre/urls.py
+++ theatre/urls.py
@@ -1,5 +1,21 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-urlpatterns = []
+from .views import (
+    GenreViewSet,
+    ActorViewSet,
+    PlayViewSet,
+    PerformanceViewSet,
+    TheatreHallViewSet,
+)
+
+router = routers.DefaultRouter()
+router.register("genres", GenreViewSet)
+router.register("actors", ActorViewSet)
+router.register("theatre_halls", TheatreHallViewSet)
+router.register("plays", PlayViewSet)
+router.register("performances", PerformanceViewSet)
+
+urlpatterns = [path("", include(router.urls))]
 
 app_name = "theatre"
--- theatre/views.py
+++ theatre/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins, status
+from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet
 
-# Create your views here.
+from .models import (
+    Genre,
+    Actor,
+    TheatreHall,
+    Play,
+    Performance,
+)
+
+from .serializers import (
+    GenreSerializer,
+    ActorSerializer,
+    TheatreHallSerializer,
+    PlaySerializer,
+    PerformanceSerializer,
+    TicketSerializer,
+    ReservationSerializer,
+)
+
+
+class GenreViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class ActorViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class TheatreHallViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = TheatreHall.objects.all()
+    serializer_class = TheatreHallSerializer
+
+
+class PlayViewSet(
+    ReadOnlyModelViewSet,
+    mixins.CreateModelMixin,
+    GenericViewSet
+):
+    queryset = Play.objects.prefetch_related("genres", "actors")
+    serializer_class = PlaySerializer
+
+
+class PerformanceViewSet(viewsets.ModelViewSet):
+    queryset = (
+        Performance.objects.all()
+        .select_related("play", "theatre_hall")
+    )
+    serializer_class = PerformanceSerializer
