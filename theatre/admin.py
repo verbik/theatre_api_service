e338b4d39799126884dd15baea11cc3449f9f99c
Yuliia <verbikyullya@gmail.com>
2024-01-02 18:23:04 +0200

    add TicketInline to Reservation admin page

--- theatre/admin.py
+++ theatre/admin.py
@@ -10,10 +10,20 @@ from .models import (
     Ticket,
 )
 
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    inlines = (TicketInline,)
+
+
 admin.site.register(TheatreHall)
 admin.site.register(Genre)
 admin.site.register(Actor)
 admin.site.register(Play)
 admin.site.register(Performance)
-admin.site.register(Reservation)
 admin.site.register(Ticket)
