4c4b0eb70490741db8e8ccfa117689a1892f11bd
Yuliia <verbikyullya@gmail.com>
2024-01-03 19:47:19 +0200

    add login, register and manage endpoints and functionality

--- user/serializers.py
+++ user/serializers.py
@@ -13,3 +13,14 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create user with encrypted password"""
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update user info with encrypted password"""
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
