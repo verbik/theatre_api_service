1c946a6964de96d35015b26e8a4f7aeb06b72eff
Yuliia <verbikyullya@gmail.com>
2024-01-04 15:18:50 +0200

    email instead of username

--- theatre/views.py
+++ theatre/views.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 
 from django.db.models import F, Count
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet
 
 from .models import (
@@ -29,6 +31,7 @@ from .serializers import (
     PlayDetailSerializer,
     ReservationListSerializer,
     PerformanceDetailSerializer,
+    PlayImageSerializer,
 )
 
 
@@ -108,7 +111,23 @@ class PlayViewSet(
         if self.action == "retrieve":
             return PlayDetailSerializer
 
+        if self.action == "upload_image":
+            return PlayImageSerializer
+
         return PlaySerializer
+
+    @action(methods=["POST"],
+            detail=True,
+            url_path="upload-image",
+            permission_classes=[IsAdminUser])
+    def upload_image(self, request, pk=None):
+        """Endpoint for uploading image to specific play"""
+        play = self.get_object()
+        serializer = self.get_serializer(play, data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class PerformanceViewSet(viewsets.ModelViewSet):
