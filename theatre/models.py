c1f72180d881fefa062ad7d9686df67d57b87116
Yuliia <verbikyullya@gmail.com>
2024-01-04 14:25:48 +0200

    add media files, field image to Play

--- theatre/models.py
+++ theatre/models.py
@@ -1,6 +1,10 @@
+import os
+import uuid
+
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.text import slugify
 
 
 class Actor(models.Model):
@@ -22,11 +26,20 @@ class Genre(models.Model):
         return self.name
 
 
+def play_image_file_path(instance, filename):
+    _, ext = os.path.splitext(filename)
+
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}.{ext}"
+
+    return os.path.join("uploads/buses/", filename)
+
+
 class Play(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     genres = models.ManyToManyField(Genre)
     actors = models.ManyToManyField(Actor)
+    image = models.ImageField(null=True, upload_to=play_image_file_path)
 
     class Meta:
         ordering = ["title"]
